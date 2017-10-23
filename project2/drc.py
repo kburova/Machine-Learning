d3c75ffad55a85fa5637b11c755367601d988fe3
Ksenia Burova <kburova@vols.utk.edu>
2017-10-23 19:23:11 -0400

    fixed bug

--- project2/drc.py
+++ project2/drc.py
@@ -4,7 +4,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats as st
 from mpl_toolkits.mplot3d import Axes3D
 
 class dr:
@@ -38,9 +37,9 @@ class dr:
 
         # Calculate proportion of varience
         p = []
-        sum_sv = sum(self.s)
+        sum_sv = sum(self.s**2)
         for i, x in enumerate(self.s):
-            p.append(sum( self.s[:i+1] ) / sum_sv)
+            p.append( sum( self.s[:i+1]**2 ) / sum_sv)
 
         plt.figure(1)
         plt.subplot(211)
@@ -58,7 +57,7 @@ class dr:
     # mxk matrix = data * V = mxn * nxk (k columns of V)
 
     def reduce(self, k):
-        self.V.resize(len(self.V), k)
+        self.V = self.V[:, :k]
         self.Z = np.matmul(self.data, self.V)
 
         plt.figure(2)
@@ -66,72 +65,62 @@ class dr:
         plt.ylabel('PC 2')
         plt.scatter(self.Z[:, 0], self.Z[:, 1], color='purple', marker='*')
         plt.savefig('scatterPCs.png')
-        plt.show()
 
-#k-means clustering
-# 1.place centroids c1...ck at random locations
-# 2. Repeat until convergence:
-#   a. for each point xi
-#       * find nearest centroid cj (compute distance for every centroid cj, arg minj D(xi,cj)  )
-#       * assign point to cluster
-#   b. for each cluster j = 1...K (each centroid)  cj(a) = 1/n sum'xi->cj' ( xi(a) ), for a=1..d
-#       * recomputes its position: new cj = mean of all points xi assigned to cluster j in previous step
-# 3. Stop when none of the cluster assignments change
 # K-means is fast O(#iter * #clusters * #instences * #dimentions)
 def kmean(k, data):
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
-    m = [] #centroids
-    m_gmeans = []
+    m = []
     num_of_iter = 0
     MAX = 10000
     randi = np.random.randint(0, len(data), size=k)
 
-    # choose random data points to be k centroids
+    # choose random data points to be k means
     for i in randi:
         m.append(data[i])
-        m_gmeans.append(st.gmean(data[i]))
 
-    plt.figure(3)
-    mt = np.transpose(np.array(m))
-    plt.scatter(mt[: ,0], mt[:, 1], color='pink', marker='o')
-    plt.savefig('kmeans.png')
-
-    # iterate until converged ot too many iterations
+    # iterate until converged or too many iterations
     while num_of_iter != MAX:
         clusters = [[] for i in range(k)]
+        b = list(range(len(data)))
         m_new = []
-        # find nearest centroid for each datapoint
-        for x in data:
+
+        # find nearest m
+        for i, x in enumerate(data):
             dist = []
             for j in m:
-                dist.append(np.linalg.norm( np.array(x) - np.array(j)))
+                dist.append(np.linalg.norm(np.array(x) - np.array(j)))
             label = dist.index(min(dist))
             clusters[label].append(x)
+            b[i] = label
 
         # compute new m
         for c in clusters:
             if len(c) == 0:
                 m_new.append(data[np.random.randint(0, len(data), size=1)[0]])
-            m_new.append(list(np.mean(c, axis=0)))
+            else:
+                m_new.append(list(np.mean(c, axis=0)))
 
         num_of_iter += 1
 
-        print(m)
-        print(m_new)
         if np.array_equal(m, np.array(m_new)):
             break
 
         m = m_new
 
-        plt.figure(3+1+num_of_iter)
-        mt = np.transpose(np.array(m))
-        plt.scatter(mt[:, 0], mt[:, 1], color='pink', marker='o')
-        name = 'km' + str(num_of_iter)
-        plt.savefig(name)
+    # plot cluster
+    colors = ['pink', 'purple', 'orange', 'yellow', 'green', 'blue', 'red']
+
+    plt.figure(3)
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+
+    for i, d in enumerate(data):
+        plt.scatter(d[0], d[1], color=colors[b[i]], marker='*')
+
+    plt.savefig('Clusters.png')
+
 
     if num_of_iter == MAX:
         print("Too many iterations", num_of_iter)
-
     else:
         print("Converged", num_of_iter)
 
@@ -141,6 +130,4 @@ def kmean(k, data):
 d = dr('under5mortalityper1000.csv')
 d.pca(2)
 
-print(len(d.Z))
-print(len(d.Z[0]))
 kmean(6, d.Z)
